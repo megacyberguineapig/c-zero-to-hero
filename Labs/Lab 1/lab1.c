14d734ffe566e72c39745335360b654c147980a9
megacyberguineapig <megacyberguineapig@protonmail.com>
2025-09-26 12:55:43 -0400

    Lab 1: update lab1.c

--- Labs/Lab 1/lab1.c
+++ Labs/Lab 1/lab1.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 int main(void){
     double fahr, celsius;
-    printf("Please input a number in Fahrenheit.");
-    scanf("%lf", &fahr)
+    printf("Please input a number in Fahrenheit. ");
+    scanf("%lf", &fahr);
     celsius = (fahr - 32.0) * 5.0 / 9.0;
-    printf(celsius);
+    printf("%.2f Fahrenheit = %.2f Celsius\n", fahr, celsius );
     return 0;
 }
